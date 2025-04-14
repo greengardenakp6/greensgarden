fb999a61e017b123276b3a5270871fea74c320f3
greengardenakp6 <akd04376@gmail.com>
2025-04-14 16:12:09 +0530

    Delete script2.js
    jnil

--- script2.js
+++
@@ -1,7 +0,0 @@
-<script>
-        document.getElementById('loginForm').addEventListener('submit', function(event) {
-            event.preventDefault();
-            sessionStorage.setItem('loggedInUser', document.getElementById('email').value);
-            window.location.href = 'home2.html';
-        });
-    </script>
